43809262de4641ff5f684d51fc9aaf20aabd0504
Gabe Isman <gabe.isman@gmail.com>
2012-10-14 14:34:00 -0400

    initial rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sparkl::Application.config.secret_token = '9a7e1ff5c55c4fdda5ef1f4468cb9caeb5563427c4b8d26735db6e881ceacbe63a77840df9a8ee7efccd033af70d8dc63f7eda24b497f58f1c00befc61dcc505'
